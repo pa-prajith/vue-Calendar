d817f17c52bd82647ffb4eb8a4bee5795aa9af26
pa-prajith <codejs100@gmail.com>
2020-11-01 22:51:29 -0800

    Completing Calender App

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -4,24 +4,25 @@
   </header>
   <div class="calendar-container">
       <div class="calendar-header">
-          <span><h2>October</h2></span>
-          <span><h2>2020</h2></span>
+          <span><h2>{{selectedMonth}}</h2></span>
+          <span><h2>{{selectedYear}}</h2></span>
       </div>
         <div class="row">
-            <div class="col"><h3>Sun</h3></div>
-            <div class="col"><h3>Mon</h3></div>
-            <div class="col"><h3>Tue</h3></div>
-            <div class="col"><h3>Wen</h3></div>
-            <div class="col"><h3>Thu</h3></div>
-            <div class="col"><h3>Fri</h3></div>
-            <div class="col"><h3>Sat</h3></div>
+            <div v-for="day in weekDays" class="col" :key="day"><h3>{{day}}</h3></div>
         </div>
         <div class="row">
-            <div v-for="num in 31" class="col" :key="num">{{num}}</div>
+            <div v-for="num1 in selectedFirstDay" class="col" :key="num1"></div>
+            <div v-for="num in lastDayOfMonth" class="col"  :class="IsToday(num) ? 'today' : ''" :key="num">{{num}}</div>
         </div>
         <div class="calendar-footer">
-            <span><i class="far fa-arrow-alt-circle-left"></i></span>
-            <span><i class="far fa-arrow-alt-circle-right"></i></span>
+            <span>
+                <i @click="prevYearHandler" class="fas fa-arrow-alt-circle-left"></i>
+                <i @click="prevMonthHandler" class="far fa-arrow-alt-circle-left"></i>
+            </span>
+            <span>
+                <i @click="nextMonthHandler" class="far fa-arrow-alt-circle-right"></i>
+                <i @click="nextYearHandler" class="fas fa-arrow-alt-circle-right"></i>
+            </span>
         </div>
   </div>
 
@@ -29,7 +30,68 @@
 
 <script>
 export default {
-    name: 'Calendar'
+    name: 'Calendar',
+
+    data() {
+        let selectedDate = new Date();
+        const today = new Date().toLocaleDateString('en-US', {day: 'numeric'});
+        let selectedMonth = new Date().toLocaleString('en-US', {month: 'long'});
+        let selectedYear = new Date().toLocaleString('en-US', {year: 'numeric'});
+        let selectedFirstDay = new Date().getDay();
+        let lastDayOfMonth = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate();
+        const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        return {
+            today,
+            selectedMonth,
+            selectedYear,
+            selectedFirstDay,
+            lastDayOfMonth,
+            selectedDate,
+            weekDays
+        };
+    },
+
+    methods: {
+        prevMonthHandler() {
+            this.getSelectedDate();
+            this.selectedDate.setMonth(this.selectedDate.getMonth() - 1);
+            this.setCalenderData()
+        },
+
+        nextMonthHandler() {
+            this.getSelectedDate();
+            this.selectedDate.setMonth(this.selectedDate.getMonth() + 1);
+            this.setCalenderData()
+        },
+
+        prevYearHandler() {
+            this.getSelectedDate();
+            this.selectedDate.setFullYear(this.selectedDate.getFullYear() - 1); 
+            this.setCalenderData()
+        },
+
+        nextYearHandler() {
+            this.getSelectedDate();
+            this.selectedDate.setFullYear(this.selectedDate.getFullYear() + 1);
+            this.setCalenderData()
+        },
+
+        getSelectedDate() {
+            this.selectedDate = new Date(this.selectedDate.getFullYear(), this.selectedDate.getMonth(), 1);
+        },
+
+        setCalenderData() {
+            this.selectedMonth = this.selectedDate.toLocaleString('en-US', {month: 'long'});
+            this.selectedYear = this.selectedDate.toLocaleString('en-US', {year: 'numeric'});
+            this.selectedFirstDay = this.selectedDate.getDay();
+            this.lastDayOfMonth = new Date(this.selectedDate.getFullYear(), this.selectedDate.getMonth() + 1, 0).getDate(); 
+        },
+
+        IsToday(d) {
+            return (this.selectedDate.getFullYear() === new Date().getFullYear() && this.selectedDate.getMonth() === new Date().getMonth() && +this.today === d)? true : false;
+        }
+    }
+
 }
 </script>
 
@@ -41,7 +103,7 @@ export default {
 
     .calendar-container {
         text-align: center;
-        width: 35%;
+        width: 45%;
         margin:0 auto;
     }
 
@@ -59,13 +121,26 @@ export default {
 
     .calendar-header {
         display: flex;
-        justify-content: space-between;
+        justify-content: center;
+    }
+
+    .calendar-header span {
+        margin-left: 20px;
     }
 
     .calendar-footer {
         display: flex;
-        justify-content: space-between;
+        justify-content: center;
         font-size: 40px;
-        margin: 10px 0;
+        margin: 10px 20px;
+    }
+
+    .calendar-footer i {
+        margin-right: 50px;
+    }
+
+    .today {
+        color: red;
+        font-weight:900;
     }
 </style>
